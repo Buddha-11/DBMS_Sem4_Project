ff796074fe0e30432a6dbed8183de1cd3b6c05c6
Arpit Anand <invoker.arpit@gmail.com>
2025-04-19 22:37:47 +0530

    backend deployment

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
-const featureRoutes = require("./routes/feature.routes")
-const taskRoutes = require("./routes/task.routes")
-const projectRoutes = require("./routes/project.routes")
-const collaborationRoutes = require("./routes/collaboration.routes")
+const featureRoutes = require("./routes/feature.routes");
+const taskRoutes = require("./routes/task.routes");
+const projectRoutes = require("./routes/project.routes");
+const collaborationRoutes = require("./routes/collaboration.routes");
+
 const app = express();
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
-}));    
+}));
+
+// Default route
 app.get('/', (req, res) => {
     res.json("Hello");
 });
@@ -26,8 +30,11 @@ app.use('/api', taskRoutes);
 app.use('/api', projectRoutes);
 app.use('/api', collaborationRoutes);
 
-// Start server
+// Export app for Vercel
+module.exports = app;
+
+// Local dev server
 if (process.env.NODE_ENV !== 'production') {
     const port = process.env.PORT || 5000;
     app.listen(port, () => console.log(`Server running on port ${port}`));
-  }
+}
